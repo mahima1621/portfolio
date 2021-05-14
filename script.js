8a0c87b38357e9be717aa257ec2584fd2e38c2aa
Mahima1621 <kumarimahima017@gmail.com>
2021-05-14 12:19:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+   $(document).ready(function(){
+    $('.checkbtn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.checkbtn i').toggleClass("active");
+    });
+  });
